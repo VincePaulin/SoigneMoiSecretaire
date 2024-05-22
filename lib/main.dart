645e24a1f794e28080b6307d1534705d17d32330
Vincent Paulin <ecniv81@gmail.com>
2024-05-22 14:03:51 +0200

    feat: debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: kDebugMode ?true :false,
       title: 'SoigneMoi Secretaire',
       theme: buildAppTheme(),
       initialRoute: '/',
